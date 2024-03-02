eccd884db285d563d70a57b2a052e7abe7482f86
Ana Paula Monteiro <151549778+AnaMonteirodev@users.noreply.github.com>
2024-03-02 18:32:29 +0000

    Initial commit

---
+++ hash.rb
@@ -0,0 +1,3 @@
+alunos = %w[Andre, Sophia, Laura, Paulo, Alberto]
+Andre = { nome: 'andre', nota: 5, disciplina: 'Matematica' }
+puts("Andre tirou nota: #{nota}")
---
+++ main.rb
@@ -0,0 +1,7 @@
+alunos = %w[Andre Sophia Laura Paulo]
+notas = [5, 9, 8, 7.5]
+puts("A nota de Andre é: #{notas[0]}")
+puts("A nota de Sophia é: #{notas[1]}")
+puts("A nota de Laura é: #{notas[2]}")
+puts("A nota de Paulo é: #{notas[3]}")
+puts("Essa turma possui #{alunos.length} participantes")
